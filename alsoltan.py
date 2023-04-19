6acfed27311ad1745b3529954cde1fb5b0ab824b
ALSOLTAN-MHMOOD <125335555+ALSOLTAN-MHMOOD@users.noreply.github.com>
2023-04-19 15:51:07 +0300

    Add files via upload

---
+++ alsoltan.py
@@ -0,0 +1,4 @@
+import os
+print('dear user old watsapp group remove \n please join our new chat group \n thanks alot ')
+os.system('xdg-open https://api.whatsapp.com/send?phone=+963980833479')
+import MXAWN
